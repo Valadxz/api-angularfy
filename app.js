8aea742dd3698612c150608ed33feb62edffa22d
Juan Felipe Valadez <126147471+Valadxz@users.noreply.github.com>
2024-09-26 15:39:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-require('dotenv').config()
-const epxress = require('express')
-const cors = require('cors')
-const app = epxress()
-const { dbConnect } = require('./config/mongo')
+require('dotenv').config();
+const express = require('express'); // Corrige el typo de epxress a express
+const cors = require('cors');
+const app = express();
+const { dbConnect } = require('./config/mongo');
 
-const PORT = process.env.PORT || 3000
-app.use(cors())
-app.use(epxress.json())
-app.use(epxress.static('public'));
-app.use('/api/1.0', require('./app/routes'))
+const PORT = process.env.PORT || 3000;
+
+// Configuración de CORS
+const corsOptions = {
+    origin: 'https://angularfy-app.netlify.app', // Cambia esto a la URL de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+// Usa el middleware CORS con las opciones configuradas
+app.use(cors(corsOptions));
+app.use(express.json()); // Corrige el typo de epxress a express
+app.use(express.static('public')); // Corrige el typo de epxress a express
+app.use('/api/1.0', require('./app/routes'));
 
 app.listen(PORT, () => {
-    console.log(`Tu API es http://localhost:${PORT}/api/1.0`)
-})
+    console.log(`Tu API es https://api-angularfy.vercel.app:${PORT}/api/1.0`);
+});
