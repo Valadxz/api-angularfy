830dc61e4f4e55f856d2b11d2f80937e61a33ee0
Juan Felipe Valadez <126147471+Valadxz@users.noreply.github.com>
2024-10-13 14:41:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 require('dotenv').config();
-const express = require('express'); // Corregido "epxress" a "express"
+const express = require('express');
 const cors = require('cors');
 const app = express();
 const { dbConnect } = require('./config/mongo');
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // Configuración de CORS
 app.use(cors({
   origin: 'https://angularfy-app.netlify.app', // Permite este dominio
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'custom_header'], // Agrega custom_header aquí
+  allowedHeaders: ['Content-Type', 'Authorization', 'custom_header'], // Agrega custom_header aquí si lo usas
 }));
 
-app.use(express.json()); // Corregido "epxress" a "express"
-app.use(express.static('public')); // Corregido "epxress" a "express"
+app.use(express.json());
+app.use(express.static('public'));
 app.use('/api/1.0', require('./app/routes'));
 
 app.listen(PORT, () => {
-    console.log(`Tu API está corriendo en https://api-angularfy.onrender.com:${PORT}/api/1.0`);
+    console.log(`Tu API está corriendo en el puerto ${PORT}`);
 });
